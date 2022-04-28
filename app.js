9c81d7f5101346748327a2c8b660944d084c0dac
hannahnewton <hnewton17@yahoo.com>
2022-04-28 17:07:36 -0400

    update app

--- app.js
+++ app.js
@@ -1,12 +1,11 @@
 const toggleButton = document.getElementsByClassName('toggleButton')[0];
 const NavBarLinks = document.getElementsByClassName('NavBarLinks')[0];
 
+toggleButton.addEventListener('click', ()=>{
+    NavBarLinks.classList.toggle('active')
+})
+
 function handle_form_submission() {
     alert('Submitted, Thank You!');
     return false;
-
-
-    toggleButton.addEventListener('click', () => {
-        NavBarLinks.classList.toggle('active')
-    })
 }
